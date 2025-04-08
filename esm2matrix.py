b6d78c6f01a90c80ec05635b1c501660213c64b3
saanvi2810 <saanviaima@gmail.com>
2025-04-08 18:49:16 -0400

    Create esm2matrix.py
    esm2 matrix draft code

---
+++ esm2matrix.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+from transformers import EsmForProtein, EsmTokenizer
+from torch.nn.functional import cosine_similarity
+
+model_name = "facebook/esm2_t33_650M_UR50D"
+tokenizer = EsmTokenizer.from_pretrained(model_name)
+model = EsmForProtein.from_pretrained(model_name)
+model.eval()
+
+def get_embeddings(sequence):
+    sequence = ' '.join(list(sequence))
+    inputs = tokenizer(sequence, return_tensors="pt", add_special_tokens=True)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+        embeddings = outputs.last_hidden_state[0] 
+
+    return embeddings[1:-1]  #remove CLS and EOS tokens
+
+def get_dynamic_substitution_matrix(seq_a, seq_b):
+    emb_a = get_embeddings(seq_a)  #(m, d)
+    emb_b = get_embeddings(seq_b)  #(n, d)
+
+    #normalize embeddings
+    emb_a_norm = emb_a / emb_a.norm(dim=1, keepdim=True)
+    emb_b_norm = emb_b / emb_b.norm(dim=1, keepdim=True)
+
+    #cosine similarity matrix computation (m, n) 
+    similarity_matrix = torch.matmul(emb_a_norm, emb_b_norm.T)
+
+    return similarity_matrix.numpy()
